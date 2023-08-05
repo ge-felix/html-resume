9e7aafbea2c7466b1be0b3e8642f7de1ecfcfd58
ge-felix <1george.1995@googlemail.com>
2023-08-05 19:23:42 +0100

    calling visitorcounterapi

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
-// script.js
+// Function to call the API Gateway and update the visitor counter
+async function updateVisitorCounter() {
+  const apiUrl = "https://v96af7p5q4.execute-api.eu-west-2.amazonaws.com/myapi-stage/execute"; // Replace with your API Gateway URL
+  var userAgent = navigator.userAgent;
+  var browserInfo = "";
 
-// Get client's browser information
-var userAgent = navigator.userAgent;
-var browserInfo = "";
-
-if (userAgent.indexOf("Chrome") !== -1) {
+  if (userAgent.indexOf("Chrome") !== -1) {
     browserInfo = "Google Chrome";
-} else if (userAgent.indexOf("Safari") !== -1) {
+  } else if (userAgent.indexOf("Safari") !== -1) {
     browserInfo = "Safari";
-} else if (userAgent.indexOf("Firefox") !== -1) {
+  } else if (userAgent.indexOf("Firefox") !== -1) {
     browserInfo = "Mozilla Firefox";
-} else if (userAgent.indexOf("Edge") !== -1) {
+  } else if (userAgent.indexOf("Edge") !== -1) {
     browserInfo = "Microsoft Edge";
-} else if (userAgent.indexOf("IE") !== -1 || userAgent.indexOf("Trident/") !== -1) {
+  } else if (userAgent.indexOf("IE") !== -1 || userAgent.indexOf("Trident/") !== -1) {
     browserInfo = "Internet Explorer";
-} else {
+  } else {
     browserInfo = "Unknown";
+  }
+  var currentTime = new Date();
+  var timeAccessed = currentTime.toISOString();
+
+  const requestData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ browser_info: browserInfo, time_accessed: timeAccessed }),
+  };
+
+  try {
+    const response = await fetch(apiUrl, requestData);
+    const data = await response.json();
+    console.log("Response:", data);
+
+    // Assuming the API response contains the total number of rows as a property named "total_rows"
+    const visitorCounterElement = document.getElementById("visitorcounter");
+    visitorCounterElement.textContent = `Visitor Number: ${data.total_rows}`;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
-// Get current time
-var currentTime = new Date();
-var formattedTime = currentTime.toLocaleString();
-
-// Display the results in the HTML
-document.getElementById("browserInfo").textContent += browserInfo;
-document.getElementById("timeAccessed").textContent += formattedTime;
+// Call the function when the DOM content is loaded
+document.addEventListener("DOMContentLoaded", updateVisitorCounter);
